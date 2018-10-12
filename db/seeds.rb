0a0e4777f24381cd5aabb17eef1604c3681255a7
David Herrera <dacherreragu@unal.edu.co>
2018-10-12 00:38:58 -0500

    Cambiar seed para las tablas existentes

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,13 @@ Role.create(name: 'Aspirante')
 
 # Poblar la tabla University
 20.times do
-  University.create(name: Faker::University.name, city: Faker::GameOfThrones.city, department: Faker::GameOfThrones.house)
+  University.create(name: Faker::Educator.university, city: Faker::GameOfThrones.city, 
+    department: Faker::LeagueOfLegends.location)
 end
 
 # Poblar la tabla Major
 20.times do
-  Major.create(name: Faker::WorldCup.stadium)
+  Major.create(name: Faker::Educator.degree)
 end
 
 # Poblar la tabla DocumentState
@@ -39,86 +40,59 @@ TypePublication.create(name: 'Evento')
 TypePublication.create(name: 'Comentario')
 
 # Poblar la tabla Image
-5.times do
-  Image.create(link: Faker::File.file_name('foo/bar', 'baz', 'png'), #=> "foo/bar/baz.doc",
-    imageable_id: rand(1..5), imageable_type: 'User')
-end
-10.times do
-  Image.create(link: Faker::File.file_name('foo/bar', 'baz', 'jpg'), #=> "foo/bar/baz.doc",
-    imageable_id: rand(1..7), imageable_type: 'Publication')
-end
-
-# Poblar la tabla UserImage
-5.times do
-  UserImage.create(idUser: rand(1..25), idImage: rand(1..5))
-end
-
-#Poblar la tabla PublicationImage
-7.times do
-  PublicationImage.create(idPublication: rand(1..10), idImage: rand(6..12))
-end
+#5.times do
+  #Image.create(link: Faker::File.file_name('foo/bar', 'baz', 'png'), #=> "foo/bar/baz.doc",
+    #imageable_id: rand(1..5), imageable_type: 'User')
+#end
+#10.times do
+  #Image.create(link: Faker::File.file_name('foo/bar', 'baz', 'jpg'), #=> "foo/bar/baz.doc",
+    #imageable_id: rand(1..7), imageable_type: 'Publication')
+#end
 
 # Poblar la tabla Document
-5.times do
-  Document.create(link: Faker::File.file_name('foo/bar', 'baz', 'pdf'), #=> "foo/bar/baz.doc",
-    documentable_id: rand(1..5), documentable_type: 'Project')
-end
-20.times do
-  Document.create(link: Faker::File.file_name('foo/bar', 'baz', 'jpg'), #=> "foo/bar/baz.doc",
-    documentable_id: rand(1..7), documentable_type: 'Publication')
-end
+#5.times do
+  #Document.create(link: Faker::File.file_name('foo/bar', 'baz', 'pdf'), #=> "foo/bar/baz.doc",
+    #documentable_id: rand(1..5), documentable_type: 'Project')
+#end
+#20.times do
+  #Document.create(link: Faker::File.file_name('foo/bar', 'baz', 'jpg'), #=> "foo/bar/baz.doc",
+    #documentable_id: rand(1..7), documentable_type: 'Publication')
+#end
 
 #Poblar la tabla ProjectDocument
 5.times do
-  ProjectDocument.create(idProject: rand(1..20), idDocument: rand(1..5))
+  ProjectDocument.create(idState: rand(1..3), idProject: rand(1..20), approved: Faker::Boolean.boolean, 
+    year: rand(1..2018), period: rand(1..2))
 end
 
-# Poblar la tabla PublicationDocument
-7.times do
-  PublicationDocument.create(idPublication: rand(1..10), idDocument: rand(6..12))
-end
-
-# Poblar la tabla Project
+#Poblar la tabla Project
 20.times do
   Project.create(name: Faker::LeagueOfLegends.champion,
-    description: Faker::LeagueOfLegends.quote,
-    idUser: rand(1..25), supervisor: rand(1..25), idState: rand(1..3))
+    description: IndustrySegments.sub_sector,
+    idUser: rand(1..25), idSupervisor: rand(1..25), idState: rand(1..3))
 end
 
 # Poblar la tabla Publication
 10.times do
-  Publication.create(name: Faker::FamilyGuy.character,
+  Publication.create(name: Faker::Book.title,
   description: Faker::LeagueOfLegends.quote,
   startdate: Time.now,
   enddate: Time.now,
   place: Faker::LeagueOfLegends.location,
   latitude: 4.35,
   longitude: 97.28,
-  typePublication: rand(1..3),
+  idType: rand(1..3),
   idUser: rand(1..25),
   parent: nil)
 end
 
 # Poblar la tabla User
 25.times do
-  User.create(name: Faker::Pokemon.name,
-  idNumber: rand(1..999),
+  User.create(name: Faker::Name.name,
+  idNumber: rand(1..999999),
   email: Faker::Internet.email,
   beneficiary: Faker::Boolean.boolean,
   idRole: rand(1..4),
-  project: rand(1..20),
   university: rand(1..20),
   major: rand(1..20))
-end
-
-# Poblar la tabla UserMajorUniversity
-20.times do
-  UserMajorUniversity.create(idUser: rand(1..25), idUniversity: rand(1..20),
-  idMajor: rand(1..20))
-end
-
-# Poblar la tabla UserProjectDocument
-20.times do
-  UserProjectDocument.create(idUser: rand(1..25), idProjectDocument: rand(1..20),
-  description: Faker::Simpsons.quote, answer: Faker::Simpsons.location)
 end
