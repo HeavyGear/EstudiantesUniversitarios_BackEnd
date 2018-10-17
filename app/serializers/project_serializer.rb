ae3d6287706b08597a81c0297e62fc6ef5a1e56d
David Herrera <dacherreragu@unal.edu.co>
2018-10-17 03:34:40 -0500

    Annotate en Master

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id               :integer          not null, primary key
+#  name             :string
+#  description      :text
+#  user_id          :integer
+#  supervisor_id    :integer
+#  project_state_id :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :description, :user_id, :supervisor_id, :project_state_id
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,14 +2,15 @@
 #
 # Table name: users
 #
-#  id          :integer          not null, primary key
-#  name        :string
-#  idNumber    :integer
-#  email       :string
-#  beneficiary :boolean
-#  role_id     :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string
+#  idNumber        :integer
+#  email           :string
+#  beneficiary     :boolean
+#  password_digest :string
+#  role_id         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 class UserSerializer < ActiveModel::Serializer
