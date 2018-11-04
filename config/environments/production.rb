bf8914ae328bba7e8d1fbf02562b80e0e224b701
David Herrera <dacherreragu@unal.edu.co>
2018-11-04 16:37:44 -0500

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # For mailing
-  config.action_mailer.default_url_options = { host: "unipastas-back.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "unipastas-frontend.herokuapp.com" }
   config.action_mailer.asset_host = 'unipastas-back.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
